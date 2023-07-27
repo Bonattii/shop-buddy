9275d49614110957002000da0bdfbbe1846ff0a8
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-07-26 17:25:23 -0700

    Create user schemas

---
+++ server/src/modules/user/user.schema.ts
@@ -0,0 +1,64 @@
+import { z } from 'zod'
+import { buildJsonSchemas } from 'fastify-zod'
+
+// Basic user schema
+const userCore = {
+  email: z
+    .string({ required_error: 'Email is required', invalid_type_error: 'Email must be a string' })
+    .email(),
+  name: z.string(),
+  username: z
+    .string()
+    .max(16, 'Username must be less than 16 characters')
+    .min(8, 'Username must be at least 8 characters'),
+  phone: z.string().max(10, 'Phone must be less than 10 characters'),
+}
+
+// Schema for creating a new user
+const createUserSchema = z.object({
+  ...userCore,
+  password: z.string({
+    required_error: 'Password is required',
+    invalid_type_error: 'Password must be a string',
+  }),
+})
+
+// Route response for creating a new user
+const createUserResponseSchema = z.object({
+  id: z.string(),
+  ...userCore,
+})
+
+// Schema for validating the data sent on the login route
+const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email must be a string',
+    })
+    .email(),
+  password: z.string({
+    required_error: 'Password is required',
+    invalid_type_error: 'Password must be a string',
+  }),
+})
+
+// Route response for login
+const loginResponseSchema = z.object({
+  accessToken: z.string(),
+})
+
+// Types to be used on the user service
+export type CreateUserInput = z.infer<typeof createUserSchema>
+export type LoginInput = z.infer<typeof loginSchema>
+
+// Export the schemas and enable to use them by $ref
+export const { schemas: userSchemas, $ref } = buildJsonSchemas(
+  {
+    createUserSchema,
+    createUserResponseSchema,
+    loginSchema,
+    loginResponseSchema,
+  },
+  { $id: 'UserSchema' }
+)
