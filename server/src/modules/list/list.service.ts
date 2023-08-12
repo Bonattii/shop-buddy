0da9e28531ff94d233584d1d551375233c6fe81f
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-08-11 22:19:08 -0700

    Update lists return

--- server/src/modules/list/list.service.ts
+++ server/src/modules/list/list.service.ts
@@ -37,9 +37,34 @@ export async function deleteList(id: string) {
 }
 
 export async function getLists(userID: string) {
-  return await prisma.list.findMany({
-    where: {
-      userIDs: { has: userID },
-    },
-  })
+  return await prisma.list
+    .findMany({
+      where: {
+        userIDs: { has: userID },
+      },
+      include: {
+        users: {
+          select: {
+            name: true,
+            id: true,
+          },
+        },
+      },
+    })
+    .then((lists) => {
+      return lists.map((list) => {
+        return {
+          id: list.id,
+          title: list.title,
+          createdAt: list.createdAt,
+          updatedAt: list.updatedAt,
+          users: list.users.map((user) => {
+            return {
+              id: user.id,
+              name: user.name,
+            }
+          }),
+        }
+      })
+    })
 }
