1ba5e0553ea62c2144713af77c369bcc1f15c29f
Elisa <elisaformighieri.als@gmail.com>
2023-08-21 00:17:43 -0300

    fasfas

--- client/app/(routes)/page.tsx
+++ client/app/(routes)/page.tsx
@@ -6,7 +6,9 @@ import HomeHero from '@/app/components/Sections/HomeHero';
 import ContactSection from '@/app/components/Sections/ContactSection';
 
 const HomeContent = () => (
-  <div className="content-container mx-8 md:mx-12 xl:mx-24 pt-12 min-h-screen">
+  <div
+    className="bg-gradient-to-br from-indigo-950 from-% via-black 3xl:via-40% via-30% to-black to-90% sm:bg-black
+  pb-10 sm:pb-0  h-screen w-screen bg-fixed content-container mx-8 md:mx-12 xl:mx-24 pt-12 min-h-screen">
     <Header />
 
     <HomeHero />
